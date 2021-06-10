793f3a67d602564804dfc25e1929867369d159d7
unknown <oriolholguint@gmail.com>
2021-06-10 12:27:15 +0200

    Añado metodo getTransactionList el cual realiza una consulta a la base de datos y segun el usuario logeado en el programa obtiene todas sus transacciones / Añado metodo insertTransaction el cual registra en la base de datos una transaccion introducida por el usuario.

--- src/databases/Database.java
+++ src/databases/Database.java
@@ -1,11 +1,16 @@
 package databases;
 
+import elements.Crypto;
+import elements.Transaction;
 import elements.User;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import localdata.LocalData;
 /**
  * @author Oriol Holguin <oriolholguint@gmail.com>
  */
@@ -25,6 +30,8 @@ public class Database
     {
         conn.close();
     }
+    
+    //==============================REGISTER Y LOGIN==============================
     
     /**
      * Comprueba que el objeto usuario con los atributos username y password sean 
@@ -111,5 +118,72 @@ public class Database
         {
             System.out.println("Usuario no registrado");
         }
+    }
+    
+    //================================MAIN WINDOW================================
+    
+    /**
+     * Se inserta una transaccion en la base de datos enviado un objeto de Transaction.
+     * @param transaction objeto de Transaccion con la informacion a insertar.
+     * @return TRUE = si la insercion se ha podido realizar correctamente.
+     * @throws SQLException 
+     */
+    public boolean insertTransaction(Transaction transaction) throws SQLException
+    {
+        boolean correctInsert = false;
+        
+        String query = "INSERT INTO transactions (transactionDate, currency, amount, userName) VALUES(?, ?, ?, ?)";
+        
+        PreparedStatement st = conn.prepareStatement(query);
+        st.setDate(1, transaction.getTransactionDate());
+        st.setString(2, transaction.getCurrency());
+        st.setFloat(3, transaction.getAmount());
+        st.setString(4, transaction.getUser().getUsuario());
+        
+        if(st.executeUpdate() != 0)
+        {
+            correctInsert = true;
+            System.out.println("*Transaccion añadida correctamente*");
+        }
+        
+        return correctInsert;
+    }
+    
+    /**
+     * Obtiene una lista de las transacciones segun el usuario que esta logeado.
+     * @return ArrayList con objetos de Transaction cada una con su respectiva 
+     * informacion.
+     * @throws SQLException 
+     */
+    public ArrayList<Transaction> getTransactionsList() throws SQLException
+    {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        Transaction transaction;
+        int id;
+        Date transactionDate;
+        String currency;
+        float amount; 
+        
+        String query = "SELECT * FROM transactions WHERE username = ?";
+        
+        PreparedStatement st = conn.prepareStatement(query);
+        
+        st.setString(1, LocalData.user.getUserName());
+        
+        ResultSet rs = st.executeQuery();
+        
+        while(rs.next())
+        {
+            id = rs.getInt("id");
+            transactionDate = rs.getDate("transactionDate");
+            currency = rs.getString("currency");
+            amount = rs.getFloat("amount");
+            
+            transaction = new Transaction(id, transactionDate, currency, amount, LocalData.user);
+            
+            transactions.add(transaction);
+        }
+        
+        return transactions;
     }
 }
